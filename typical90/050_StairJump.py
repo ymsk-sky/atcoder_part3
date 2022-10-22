f590d00a807f4e64749a1d1c34edbcbb865b9045
ymsk-sky <ymsk.sky.95@gmail.com>
2022-10-22 15:10:06 +0900

    typical90 050 AC

---
+++ typical90/050_StairJump.py
@@ -0,0 +1,12 @@
+n, l = map(int, input().split())
+mod = 10 ** 9 + 7
+
+dp = [0] * (n + 1)
+dp[0] = 1
+for i in range(1, n + 1):
+    dp[i] += dp[i - 1]
+    dp[i] %= mod
+    if i - l >= 0:
+        dp[i] += dp[i - l]
+        dp[i] %= mod
+print(dp[n])
