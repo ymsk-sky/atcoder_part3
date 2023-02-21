bc9f06af71b53d84ad043ae952e1f87972b51ea3
ymsk-sky <ymsk.sky.95@gmail.com>
2023-02-21 21:38:20 +0900

    bootcamp medium 039 AC

--- bootcamp_medium/039_C_When_I_hit_my_pocket.py
+++ bootcamp_medium/039_C_When_I_hit_my_pocket.py
@@ -1,7 +1,10 @@
 k, a, b = map(int, input().split())
-if a + 1 < b:
-    tmp1 = k//(a + 2)*b  # 交換
-    tmp2 = k%(a + 2) + 1  # 残り
-    print(tmp1 + tmp2)
+if a + 1 >= b:
+    print(k + 1)
+    exit()
+n = k - (a - 1)
+if n > 0:
+    ans = n//2*(b - a) + a + n%2
+    print(ans)
 else:
     print(k + 1)
