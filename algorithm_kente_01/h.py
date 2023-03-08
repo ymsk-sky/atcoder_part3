3cc687cbf4c70152481ebcfc128c567c9332f4bb
ymsk-sky <ymsk.sky.95@gmail.com>
2023-03-08 22:14:19 +0900

    アルゴリズム実技検定過去問01 h AC(cheated)

--- algorithm_kente_01/h.py
+++ algorithm_kente_01/h.py
@@ -1,42 +1,42 @@
 n = int(input())
 cl = list(map(int, input().split()))
 q = int(input())
-m = -(-n//2)  # 奇数番目の個数
-# 最小値(閾値を考慮した実際の値)
-min_all = min(cl)
-min_odd = min([cl[i] for i in range(n) if i%2 == 0])
-# 閾値
-th_all = 0
-th_odd = 0
+
+on = -(-n//2)
+if n == 1:
+    ans = 0
+    for _ in range(q):
+        s = list(map(int, input().split()))
+        a = s[-1]
+        if cl[0] >= a:
+            cl[0] -= a
+            ans += a
+    print(ans)
+    exit()
+
+ml = [min([cl[i] for i in range(n) if i%2 == 1]),
+      min([cl[i] for i in range(n) if i%2 == 0])]
+ol = [0, 0]
+
 ans = 0
 for _ in range(q):
     s = list(map(int, input().split()))
     if s[0] == 1:
         x, a = s[1:]
-        if x%2 == 0:
-            if cl[x - 1] - th_all >= a:
-                cl[x - 1] -= a
-                ans += a
-                min_all = min(min_all, cl[x - 1] - th_all)
-        else:
-            th = th_all + th_odd
-            if cl[x - 1] - th >= a:
-                cl[x - 1] -= a
-                ans += a
-                min_all = min(min_all, cl[x - 1] - th)
-                min_odd = min(min_odd, cl[x - 1] - th)
+        mod = x%2
+        if cl[x - 1] - ol[mod] >= a:
+            cl[x - 1] -= a
+            ans += a
+            ml[mod] = min(ml[mod], cl[x - 1])
     elif s[0] == 2:
         a = s[1]
-        if min_odd >= a:
-            min_odd -= a
-            min_all = min(min_all, min_odd)
-            th_odd += a
-            ans += a*m
+        if ml[1] - ol[1] >= a:
+            ol[1] += a
+            ans += a*on
     else:
         a = s[1]
-        if min_all >= a:
-            min_all -= a
-            th_all += a
+        if ml[0] - ol[0] >= a and ml[1] - ol[1] >= a:
+            ol[0] += a
+            ol[1] += a
             ans += a*n
-
 print(ans)
