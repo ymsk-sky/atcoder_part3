c980c540ec1bd1c9dcc25a9ca719b1bf6050571c
ymsk-sky <ymsk.sky.95@gmail.com>
2023-04-14 21:49:14 +0900

    algo_ken_12 f input

---
+++ algorithm_kente_12/f.py
@@ -0,0 +1,11 @@
+n = int(input())
+al = list(map(int, input().split()))
+
+for _ in range(n):
+    c = int(input())
+    xl = list(map(int, input().split()))
+
+q = int(input())
+for _ in range(q):
+    d = int(input())
+    yl = list(map(int, input().split()))
